b944b38039bc2515f0ed443bf460f995325da1ce
Tedydev <Hieudat2310.bh@gmail.com>
2025-05-02 14:02:58 +0700

    Delete Console.log test

--- src/routes/posts/posts.service.ts
+++ src/routes/posts/posts.service.ts
@@ -1,14 +1,12 @@
 /* eslint-disable @typescript-eslint/no-unsafe-member-access */
 /* eslint-disable @typescript-eslint/no-unsafe-assignment */
 import { Injectable } from '@nestjs/common'
-import envConfig from 'src/shared/config'
 import { PrismaService } from 'src/shared/services/prisma.service'
 
 @Injectable()
 export class PostsService {
   constructor(private readonly prismaService: PrismaService) { }
   getPosts() {
-    console.log(envConfig.DATABASE_URL)
     return this.prismaService.post.findMany()
   }
 
