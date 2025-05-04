59352f9eafa7ced21460cc638647a2efcab42d39
Tedydev <Hieudat2310.bh@gmail.com>
2025-05-04 21:24:52 +0700

    Protect Route with Guard

--- src/routes/auth/auth.controller.ts
+++ src/routes/auth/auth.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, HttpCode, HttpStatus, Post } from '@nestjs/common'
+import { Body, Controller, HttpCode, HttpStatus, Post, UseGuards } from '@nestjs/common'
 import { AuthService } from './auth.service'
 import {
   LoginBodyDTO,
@@ -8,6 +8,7 @@ import {
   RegisterBodyDTO,
   RegisterResDTO
 } from './auth.dto'
+import { AccessTokenGuard } from 'src/shared/guards/access-token.guard'
 
 @Controller('auth')
 export class AuthController {
@@ -22,9 +23,11 @@ export class AuthController {
     return new LoginResDTO(await this.authService.login(body))
   }
 
+  @UseGuards(AccessTokenGuard)
   @Post('refresh-token')
   @HttpCode(HttpStatus.OK)
   async refreshToken(@Body() body: RefreshTokenBodyDTO) {
+    console.log('123')
     return new RefreshTokenResDTO(await this.authService.refreshToken(body.refreshToken))
   }
 }
